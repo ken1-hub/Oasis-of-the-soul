808d5ca573e8f59ed91c2c4b142bf48a3443fd64
ken1-hub <2228371952@qq.com>
2026-04-07 20:09:42 +0800

    ui v0.1

---
+++ oasis-of-the-soul/oasis_ui.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_meditation_relax_pressed() -> void:
+	print("进入冥想模块") # Replace with function body.
+
+
+func _on_button_small_game_pressed() -> void:
+	print("进入压力释放模块") # Replace with function body.
